a8ed91eeb22fae585ec80dcedbcf766b2706df44
hiep1810 <you@example.com>
2025-07-19 02:00:28 +0700

    hot fix: fix celery worker error

--- celery_worker.py
+++ celery_worker.py
@@ -2,6 +2,14 @@ from celery import Celery
 import subprocess
 import os
 
+from dotenv import load_dotenv
+
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    raise ImportError("❌ python-dotenv is not installed. Install it with `pip install python-dotenv`.")
+load_dotenv()
+
 celery_app = Celery(
     "tasks",
     broker="redis://localhost:6379/0",
@@ -10,6 +18,12 @@ celery_app = Celery(
 
 WHISPER_CLI = os.environ.get("WHISPER_CLI")
 MODEL = os.environ.get("MODEL")
+
+if not WHISPER_CLI:
+    raise ValueError("❌ WHISPER_CLI is not set! Please check your .env file.")
+
+if not MODEL:
+    raise ValueError("❌ MODEL is not set! Please check your .env file.")
 
 @celery_app.task
 def transcribe_task(input_path, output_path, language, format):
